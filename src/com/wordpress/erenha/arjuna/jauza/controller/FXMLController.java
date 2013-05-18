d64bfaeb5d93c15a7f0ccff5604557c6910d47c6
RN Hindarwan <ntboy9191@gmail.com>
2013-05-18 23:46:17 +0700

    fix bug + optimize

--- src/com/wordpress/erenha/arjuna/jauza/controller/FXMLController.java
+++ src/com/wordpress/erenha/arjuna/jauza/controller/FXMLController.java
@@ -58,9 +58,6 @@ public class FXMLController implements Initializable {
     private TableView<ExtractedIndividual> tableExtInd; // Value injected by FXMLLoader
     //NON FXML
     private ObservableList<ExtractedIndividual> extractedIndividual;
-//    private boolean isSelectionMode = false;
-//    private String selectedElement = "";
-//    private String htmlDoc = "";
     private WebEngine engine;
 
     @Override // This method is called by the FXMLLoader when initialization is complete
@@ -81,7 +78,6 @@ public class FXMLController implements Initializable {
         columnID.setCellValueFactory(new PropertyValueFactory("id"));
         columnProperty.setCellValueFactory(new PropertyValueFactory("property"));
         ArrayList<ExtractedIndividual> l = new ArrayList<>();
-        l.add(new ExtractedIndividual("Jacob", "Smith", "jacob.smith@example.com"));
         extractedIndividual = FXCollections.observableList(l);
 
         tableExtInd.setItems(extractedIndividual);
@@ -115,17 +111,16 @@ public class FXMLController implements Initializable {
     public void inspect(ActionEvent event) {
         System.out.println("[INFO] inspect mode");
         engine.executeScript(INSPECT_SCRIPT);
-//        selectedElement = "";
     }
 
     @FXML //versi 2
     public void getSelectedElement(ActionEvent event) {
-        Integer length = (Integer) engine.executeScript("document.querySelectorAll('[class=\\\"sg_selected\\\"]').length");
-        System.out.println(length);
+        Integer length = (Integer) engine.executeScript("document.querySelectorAll('[class*=\\\"sg_selected\\\"]').length");
+        System.out.println("[INFO] selected : " + length);
         if (length > 0) {
             ArrayList<ExtractedIndividual> list = new ArrayList<>();
             for (int i = 0; i < length; i++) {
-                Element selectedElement = (Element) engine.executeScript("document.querySelectorAll('[class=\\\"sg_selected\\\"]').item(" + i + ")");
+                Element selectedElement = (Element) engine.executeScript("document.querySelectorAll('[class*=\\\"sg_selected\\\"]').item(" + i + ")");
                 String id = selectedElement.getAttribute("jfxid");
                 String content = selectedElement.getTextContent();
                 String link = selectedElement.getAttribute("href");
@@ -133,9 +128,9 @@ public class FXMLController implements Initializable {
                 System.out.println("content : " + content);
                 System.out.println("link : " + link);
                 NodeList links = selectedElement.getElementsByTagName("a");
-                for (int j = 0; i < links.getLength(); i++) {
+                for (int j = 0; j < links.getLength(); j++) {
                     Element l = (Element) links.item(j);
-                    System.out.println("link : " + l.getAttribute("href"));
+                    System.out.println("child link : " + l.getAttribute("href"));
                 }
                 ExtractedIndividual ei = new ExtractedIndividual(id, content.trim(), "");
                 list.add(ei);
@@ -182,19 +177,10 @@ public class FXMLController implements Initializable {
         engine.setOnAlert(new EventHandler<WebEvent<String>>() {
             @Override
             public void handle(WebEvent<String> e) {
-                //if make selection mode is completed
-//                if (e.getData().equals("jauza:001")) {
-                //document ready for selection
-                //than parse the code to html string via jsoup
-                // ouch but i can code with standard, goodbye jsoup C.U.
-//                    System.out.println("[INFO] jauza:001");
-//                } else {
                 if (e.getData().equals("jauza:002")) { //clear selection
-//                    selectedElement = "";
                     System.out.println("[INFO] clear selection");
                 } else {
-//                    selectedElement = e.getData();
-//                    System.out.println("[INFO] selected element: " + selectedElement);
+                    
                 }
             }
         });
@@ -229,82 +215,4 @@ public class FXMLController implements Initializable {
 
     }
     
-//    @FXML
-//    public void getSelectedElement1(ActionEvent event) {
-        //with standard
-//        if (!selectedElement.isEmpty()) {
-//            String[] els = selectedElement.split(",");
-//            ArrayList<ExtractedIndividual> list = new ArrayList<>();
-//            for (String el : els) {
-//                //with JS Standard++
-//                Element a = (Element) engine.executeScript("document.querySelector('[jfxid=\"" + el + "\"]')");
-//
-//                System.out.println("id : " + el);
-//                System.out.println("content : " + a.getTextContent());
-//                System.out.println("link : " + a.getAttribute("href"));
-//                NodeList link = a.getElementsByTagName("a");
-//                for (int i = 0; i < link.getLength(); i++) {
-//                    Element l = (Element) link.item(i);
-//                    System.out.println("link : " + l.getAttribute("href"));
-//                }
-//                ExtractedIndividual ei = new ExtractedIndividual(el, a.getTextContent().trim(), "");
-//                list.add(ei);
-
-                //with Jquery
-                //                JQueryUtil.executejQuery(engine,
-                //                        "var txt = $('[jfxid=\"" + el +"\"]').text();"
-                //                        + "alert(txt);");
-
-
-
-                //with JS standard
-                //                Element a = (Element) engine.executeScript("document.getElementById('" + el + "')");
-                ////                                Element a = (Element) engine.executeScript("jQuery('#" + el + "').attr('jfxid')");
-                //
-                //                System.out.println("id : " + el);
-                //                System.out.println(a.getTextContent());
-                //                System.out.println("link : " + a.getAttribute("href"));
-                //                NodeList link = a.getElementsByTagName("a");
-                //                for (int i = 0; i < link.getLength(); i++) {
-                //                    Element l = (Element) link.item(i);
-                //                    System.out.println("link : " + l.getAttribute("href"));
-                //                }
-                //                ExtractedIndividual ei = new ExtractedIndividual(el, a.getTextContent().trim(), "");
-                //                list.add(ei);
-//            }
-//            extractedIndividual = FXCollections.observableList(list);
-//
-//            tableExtInd.setItems(extractedIndividual);
-//        }
-
-        //with 3rd party lib
-//            org.jsoup.nodes.Document doc = Jsoup.parse(htmlDoc);
-//            String[] els = selectedElement.split(",");
-//            for (String el : els) {
-//                String text = doc.select("#" + el).text();
-//                System.out.println(text);
-//            }
-//    }
-    
-//        @FXML
-//    public void viewSource(ActionEvent event) {
-////        System.out.println(getStringFromDocument(engine.getDocument()));
-//    }
-    //method to convert Document to String
-//    private String getStringFromDocument(Document doc) {
-//        try {
-//            DOMSource domSource = new DOMSource(doc.getElementsByTagName("body").item(0));
-//            StringWriter writer = new StringWriter();
-//            StreamResult result = new StreamResult(writer);
-//            TransformerFactory tf = TransformerFactory.newInstance();
-//            Transformer transformer = tf.newTransformer();
-//            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-//            transformer.transform(domSource, result);
-//            return writer.toString();
-//        } catch (TransformerException ex) {
-//            ex.printStackTrace();
-//            return null;
-//        }
-//    }
-
 }
